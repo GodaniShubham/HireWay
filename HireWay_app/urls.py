a6fd8e6c90031ea0940ce26a4dcb8fc40bcf769d
chavdarajveersinh <rajveer.studenttdb@gmail.com>
2025-08-30 12:00:42 +0530

    student dashboard add

--- HireWay_app/urls.py
+++ HireWay_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.student_dashboard, name='student_dashboard'),
-    path('job_list/', views.job_list, name='job_applications'),
-    path('apply/<int:job_id>/', views.apply_job, name='apply_job'),
+    path("company/", views.company_dashboard, name="company_dashboard"),
+    path('jobs/', views.job_applications, name='job_applications'),
+
 ]
